e47d34a7f324702fca138888798dd1fdd0dfd2d8
Michael <58229534+Mike7608@users.noreply.github.com>
2023-07-13 12:51:45 +0300

    Add files via upload

---
+++ basic_word.py
@@ -0,0 +1,32 @@
+class BasicWord:
+
+    def __init__(self, word: str, subwords: list):
+        """
+        Процедура инициализации BasicWord
+        :param word: исходное слово
+        :param subwords: набор (список) допустимых подслов
+        """
+        self.word = word
+        self.subwords = subwords
+
+    def is_correct(self, player_word: str):
+        """
+        Проверка введенного слова в списке допустимых подслов
+        :param player_word:
+        :return: True / False
+        """
+        return player_word in self.subwords
+
+    def get_subword_count(self):
+        """
+        Подсчет количества подслов
+        :return: int значение
+        """
+        return len(self.subwords)
+
+    def __repr__(self):
+        """
+        Функция для отображения полей класса
+        :return: строка с названиями полей класса и их значениями
+        """
+        return f"word = {self.word}, subwords = {self.subwords}"
